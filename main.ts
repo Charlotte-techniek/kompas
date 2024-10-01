33c00e568ec6649237c27ac7341220229704046a
Charlotte-techniek <charlotte.malfliet99@gmail.com>
2024-10-01 10:25:50 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,47 @@
+let graden = 0
+basic.forever(function () {
+    graden = input.compassHeading()
+})
+basic.forever(function () {
+    if (input.compassHeading() > 45 && input.compassHeading() < 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() > 135 && input.compassHeading() < 180) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (input.compassHeading() > 225 && input.compassHeading() < 315) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (input.compassHeading() > 315 && input.compassHeading() <= 360) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.compassHeading() > 45 && input.compassHeading() <= 360) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
